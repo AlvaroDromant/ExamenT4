d621fa9b4396b901173c31dae8b91628ec9bec13
Alvaro <alvarodromantligero@alumnos.tuniversformacion.es>
2023-12-04 08:58:17 +0100

    creamos la clase vehiculo

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const resultadocontainer = document.createElement('div');
 document.body.appendChild(resultadocontainer);
 
-function mostrarResultado(mensaje){
+export function mostrarResultado(mensaje){
  resultadocontainer.innerHTML = `<p>${mensaje}</p>`
 }
--- scripts/vehiculo.js
+++ scripts/vehiculo.js
@@ -1,3 +1,90 @@
+import { mostrarResultado } from "./index.js"
+
 class Vehiculo {
-    
+    #marca
+    #modelo
+    #color
+    #fabricacion
+    #cilindrada
+
+constructor(marca, modelo, color, fabricacion, cilindrada){
+    this.#marca = marca;
+    this.#modelo = modelo;
+    this.#color = color;
+    this.#fabricacion = fabricacion;
+    this.#cilindrada;
+}
+
+mostrarDatos(){
+    const datos = `
+    La marca es -> ${marca} <br>
+    El modelo es -> ${modelo} <br>
+    El color es -> ${color} <br>
+    El año de fabricacion es -> ${fabricacion} <br>
+    La cilindrada es -> ${cilindrada}
+    `;
+
+    mostrarResultado(`<p>${datos}</p>`)
+}
+
+acelerar(velocidad){
+    const mensaje = `El coche ha acelerado hasta ${velocidad}`;
+    mostrarResultado(`<p>${mensaje}</p>`);
+}
+
+arrancar(){
+    const mensaje = `El coche de marca ${marca} y modelo ${modelo} ha arrancado`;
+    mostrarResultado(`<p>${mensaje}</p>`);
+
+}
+
+frenar(){
+    const mensaje = `El coche de marca ${marca} y modelo ${modelo} ha frenado`;
+    mostrarResultado(`<p>${mensaje}</p>`);
+}
+
+get marca () {
+  return this.#marca;
+}
+
+set marca (marca){
+    this.#marca = marca;
+}
+
+get modelo (){
+    return this.modelo;
+}
+
+set modelo (modelo){
+    this.#modelo = modelo
+}
+
+get color (){
+    return this.#color;
+}
+
+set color (color){
+    this.#color = color;
+}
+
+get fabricacion(){
+    return this.#fabricacion;
+}
+
+set fabricacion (fabricacion){
+    this.#fabricacion = fabricacion;
+}
+
+get cilindrada(){
+    return this.#cilindrada;
+}
+
+set cilindrada(cilindrada){
+    this.#cilindrada = cilindrada;
+}
+
+
+
+
+
 }
